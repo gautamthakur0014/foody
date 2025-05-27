62860e1d58e428689fac6b7b200ec3913d2d8479
Gautam <gautamthakur0014@gmail.com>
2025-05-27 18:02:16 +0530

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"], //can be used inside src folder on file ext -{ }
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
