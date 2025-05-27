92c0fda66917fa2fb8cf6ec0ef37d32464538fb8
Gautam <gautamthakur0014@gmail.com>
2025-05-27 17:58:16 +0530

    redux added

--- src/app.js
+++ src/app.js
@@ -1,21 +1,27 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import { createBrowserRouter, RouterProvider ,Outlet} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Error from "./components/Error";
 import ResturantMenu from "./RestaurantMenu";
+import Login from "./components/Login";
+import Cart from "./components/Cart";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
 const AppLayout = () => {
   return (
-    <div className="app">
-      <Header />
-      <Outlet />
-    </div>
+    <Provider store={appStore}>
+      <div className="app">
+        <Header />
+        <Outlet />
+      </div>
+    </Provider>
   );
 };
 
@@ -23,27 +29,34 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    children:[
+    children: [
       {
         path: "/",
-        element: <Body/>,
+        element: <Body />,
       },
-       {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-  {
-    path: "/resturants/:resId",
-    element: <ResturantMenu/>,
-  },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/resturants/:resId",
+        element: <ResturantMenu />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
     ],
-    errorElement: <Error/>,
+    errorElement: <Error />,
   },
- 
 ]);
 
 root.render(<RouterProvider router={appRouter} />);
