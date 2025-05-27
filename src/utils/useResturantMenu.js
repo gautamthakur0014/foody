5aa82230ff7da2e4abc6f9c6f1a15315919a67c6
Gautam <gautamthakur0014@gmail.com>
2025-05-27 17:53:47 +0530

    API updated

--- src/utils/useResturantMenu.js
+++ src/utils/useResturantMenu.js
@@ -13,10 +13,25 @@ const useResturantMenu = (resId) => {
     );
 
     const json = await data.json();
-    const cards =
-      json?.data?.cards[4].groupedCard?.cardGroupMap?.REGULAR.cards[1].card
-        ?.card?.itemCards;
-    setresMenu(cards);
+    // console.log(json);
+
+    const cards = json?.data?.cards.find(
+      (items) => items.groupedCard?.cardGroupMap?.REGULAR.cards
+    );
+    const ocard = cards.groupedCard?.cardGroupMap?.REGULAR.cards;
+    // console.log(ocard);
+
+    // const ncard = ocard.find((item) => item.card.card.itemCards);
+    const ncard1 = ocard.filter(
+      (item) =>
+        item.card.card["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+    console.log(ncard1);
+    // const rcard = ncard.card?.card?.itemCards;
+    // console.log(rcard);
+
+    setresMenu(ncard1);
   };
 
   return resMenu;
