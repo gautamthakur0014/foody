01b8acf9707be298d2d120c758ffcb4c0e4e02d1
Gautam <gautamthakur0014@gmail.com>
2025-05-28 18:21:44 +0530

    implemented  lazy loading

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDom from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import Contact from "./components/Contact";
-import About from "./components/About";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Error from "./components/Error";
 import ResturantMenu from "./components/RestaurantMenu";
-
 import Login from "./components/Login";
 import Cart from "./components/Cart";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
+const About = lazy(() => import("./components/About"));
+const Contact = lazy(() => import("./components/Contact"));
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
@@ -37,15 +37,23 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About />,
+        element: (
+          <Suspense fallback={<div>Loading About...</div>}>
+            <About />
+          </Suspense>
+        ),
       },
       {
         path: "/contact",
-        element: <Contact />,
+        element: (
+          <Suspense fallback={<div>Loading Contact...</div>}>
+            <Contact />
+          </Suspense>
+        ),
       },
       {
         path: "/resturants/:resId",
-        element: <ResturantMenu/>,
+        element: <ResturantMenu />,
       },
       {
         path: "/login",
