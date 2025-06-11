f2755605abfab9cdf31fa94e6176ab7e23d985ba
Gautam <gautamthakur0014@gmail.com>
2025-06-11 21:22:00 +0530

    api updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -16,7 +16,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const response = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await response.json();
     // console.log(json);
