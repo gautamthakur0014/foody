8ec40acc1d179dbf74ffcf259ef94261673fdd75
Gautam <gautamthakur0014@gmail.com>
2025-06-16 22:55:30 +0530

    custom screen value added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"], //can be used inside src folder on file ext -{ }
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "500px", // custom value for md
+        // md: "750px",
+        // lg: "1024px",
+        // xl: "1280px",
+      },
+    },
   },
   plugins: [],
 };
