30de649c3f14423738bc192ab0587b44a9df857d
Gautam <gautamthakur0014@gmail.com>
2025-06-16 22:59:54 +0530

    console.log removed

--- src/utils/useResturantMenu.js
+++ src/utils/useResturantMenu.js
@@ -11,23 +11,16 @@ const useResturantMenu = (resId) => {
     const data = await fetch(`/api/swiggy-menu?resId=${resId}`);
 
     const json = await data.json();
-    // console.log(json);
 
     const cards = json?.data?.cards.find(
       (items) => items.groupedCard?.cardGroupMap?.REGULAR.cards
     );
     const ocard = cards.groupedCard?.cardGroupMap?.REGULAR.cards;
-    // console.log(ocard);
-
-    // const ncard = ocard.find((item) => item.card.card.itemCards);
     const ncard1 = ocard.filter(
       (item) =>
         item.card.card["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
-    console.log(ncard1);
-    // const rcard = ncard.card?.card?.itemCards;
-    // console.log(rcard);
 
     setresMenu(ncard1);
   };
