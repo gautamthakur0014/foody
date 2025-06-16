1b98697999d89fa3312e9161a1d19e24105403c9
Gautam <gautamthakur0014@gmail.com>
2025-06-16 23:07:15 +0530

    UI fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,5 @@
 import { useState } from "react";
 
-
-
 const Contact = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -18,16 +16,16 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
- //logic
+    //logic
     alert("Message sent! We'll get back to you soon.");
   };
 
   return (
-    <div className="min-h-screen bg-black dark:bg-sky-100 text-black dark:text-white px-6 py-12 flex flex-col items-center">
+    <div className="min-h-scree bg-sky-100 text-black dark:text-white px-6 py-12 flex flex-col items-center">
       <h1 className="text-3xl md:text-4xl font-bold mb-4 text-black">
         Contact Us
       </h1>
-      <p className="text-center text-black dark:black max-w-xl mb-10">
+      <p className="text-center text-black max-w-xl mb-10">
         We'd love to hear from you! Whether you have a question about our menu,
         delivery, or anything else — our team is ready to answer all your
         questions.
@@ -61,7 +59,7 @@ const Contact = () => {
         {/* Contact Form */}
         <form
           onSubmit={handleSubmit}
-          className="bg-gray-50 dark:bg-sky-200 p-6 rounded-lg shadow-lg space-y-4"
+          className="bg-sky-200 p-6 rounded-lg shadow-lg space-y-4"
         >
           <div>
             <label className="block mb-1 font-medium" htmlFor="name">
@@ -73,7 +71,7 @@ const Contact = () => {
               name="name"
               required
               onChange={handleChange}
-              className="w-full px-4 py-2 border   rounded-md bg-white   focus:outline-none focus:ring-2 focus:ring-orange-400"
+              className="w-full px-4 py-2 border   rounded-md bg-white   focus:outline-none focus:ring-2 focus:to-blue-700"
             />
           </div>
           <div>
@@ -86,7 +84,7 @@ const Contact = () => {
               name="email"
               required
               onChange={handleChange}
-              className="w-full px-4 py-2 border   rounded-md bg-white   focus:outline-none focus:ring-2 focus:ring-orange-400"
+              className="w-full px-4 py-2 border   rounded-md bg-white   focus:outline-none focus:ring-2 focus:to-blue-700"
             />
           </div>
           <div>
@@ -99,7 +97,7 @@ const Contact = () => {
               rows="5"
               required
               onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-md bg-white  focus:outline-none focus:ring-2 focus:ring-orange-400"
+              className="w-full px-4 py-2 border rounded-md bg-white  focus:outline-none focus:ring-2 focus:to-blue-700"
             />
           </div>
           <button
